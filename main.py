eaa6230e7980dc337cd750608ee67947f4e47308
danipaniii <danielpp1450@gmail.com>
2024-06-26 00:50:41 +0200

    finished script + test add graph to page

--- main.py
+++ main.py
@@ -1,1 +1,77 @@
-print("test")
+import math
+import csv
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+data = {}
+
+min = float("inf")
+max = float("-inf")
+
+n = 0
+sum = 0
+sd_sum = 0
+
+probabilities = {}
+
+if __name__ == "__main__":
+    print("starting generation of statistics...")
+
+    with open('data.csv', mode='r') as file:
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            key, value = int(row[0]), int(row[1])
+            data[key] = value
+
+    print("Read data...")
+    
+    for k in data.keys():
+        n += data[k]
+        sum += k * data[k]
+
+        if k < min:
+            min = k
+
+        if k > max:
+            max = k
+
+    mean = sum / n
+
+    for k in data.keys():
+        sd_sum += math.pow(k - mean, 2) * data[k]
+
+    sd = math.sqrt(sd_sum / n)
+
+    print("Mean", mean)
+    print("Sd", sd)
+
+    c = min
+
+    while c <= max:
+        za = c-1
+        zb = c+1
+
+        za = (za - mean) / sd
+        zb = (zb - mean) / sd
+
+        prob = (stats.norm.cdf(zb) - stats.norm.cdf(za)) * 100
+        probabilities[c] = prob
+
+        c += 1
+
+    values = list(probabilities.keys())
+    probs = list(probabilities.values())
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(values, probs, marker="o")
+
+    plt.xlabel("Cycle Length (days)")
+    plt.ylabel("Probability in %")
+    plt.title("Probability Distribution of Cycle Lengths")
+
+    plt.grid(True)
+    #plt.show()
+
+    plt.savefig("./docs/images/graph.png")
+
+    
